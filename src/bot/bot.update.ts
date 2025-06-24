f770e8bbe41722a04d33320c7ee9761fbe8bf616
Anton <a.soroka@terabit.ai>
2025-06-24 13:08:45 +0300

    inlain btn

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,8 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Command, Ctx, Help, InjectBot, Message, On, Start, Update, } from 'nestjs-telegraf';
+import {
+  Command,
+  Ctx,
+  Help,
+  InjectBot,
+  Message,
+  On,
+  Start,
+  Update,
+} from 'nestjs-telegraf';
 import { GigachatService } from 'src/gigachat/gigachat.service';
 import { Context, Telegraf } from 'telegraf';
-import { Agent } from 'node:https';
 
 @Update()
 @Injectable()
@@ -10,59 +18,96 @@ export class BotUpdate {
   constructor(
     @InjectBot()
     private readonly bot: Telegraf<Context>,
-    @Inject(GigachatService) 
-    private readonly ai: GigachatService
+    @Inject(GigachatService)
+    private readonly ai: GigachatService,
   ) {}
 
   @Start()
   async startCommand(@Ctx() ctx: Context) {
-    await ctx.reply('Привет! Я бот на dd.news! Через меня можно получить информацию или открыть приложение',{
-      reply_markup: {
-        inline_keyboard: [
-          [{
-            text: 'Запустить приложение',
-            web_app: { url: 'https://mini-app-dd-news.vercel.app/' }
-          }]
-        ]
-      }
-    });
+    await ctx.reply(
+      'Привет! Я бот на dd.news! Через меня можно получить информацию или открыть приложение',
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Запустить приложение',
+                web_app: { url: 'https://mini-app-dd-news.vercel.app/' },
+              },
+            ],
+          ],
+        },
+      },
+    );
   }
 
   @Help()
   async onHelp(@Ctx() ctx: Context) {
-    await ctx.reply('Доступные команды:\n/start - Начать работу\n/help - Помощь\n/site - наш сайт\n/open_app - приложене\n\nEmail: test@mail.ru');
+    await ctx.reply(
+      'Доступные команды:\n/start - Начать работу\n/order - заказать\n/help - Помощь\n/site - наш сайт\n/open_app - приложене\n\nEmail: test@mail.ru\n\nЕще я могу дать ответ на вопросы, попробуй написать мне сообщение',
+    );
   }
 
-  @Command("site")
+  @Command('site')
   async siteCommand(@Ctx() ctx: Context) {
     await ctx.reply('Читайте новости на нашем сайте: https://dd.news/');
   }
 
-  @Command("open_app")
+  @Command('open_app')
   async openAppCommand(@Ctx() ctx: Context) {
     await ctx.reply('Вот кнопка для открытия', {
       reply_markup: {
         inline_keyboard: [
-          [{
-            text: 'Запустить приложение',
-            web_app: { url: 'https://mini-app-dd-news.vercel.app/' }
-          }]
-        ]
-      }
+          [
+            {
+              text: 'Запустить приложение',
+              web_app: { url: 'https://mini-app-dd-news.vercel.app/' },
+            },
+          ],
+        ],
+      },
     });
   }
 
-  @Command("order")
+  @Command('order')
   async orderCommand(@Ctx() ctx: Context) {
     await ctx.reply('Заказать услуги следующие:', {
       reply_markup: {
         inline_keyboard: [
-          [{
-            text: 'Запустить приложение',
-            web_app: { url: 'https://mini-app-dd-news.vercel.app/' }
-          }]
-        ]
-      }
+          [
+            {
+              text: 'Запустить приложение',
+              web_app: { url: 'https://mini-app-dd-news.vercel.app/' },
+            },
+          ],
+          [
+            {
+              text: 'Открыть сайт',
+              url: 'https://mini-app-dd-news.vercel.app/',
+            },
+          ],
+          [
+            {
+              text: 'Запустить приложение',
+              web_app: { url: 'https://mini-app-dd-news.vercel.app/' },
+            },
+          ],
+          [
+            {
+              text: 'callback_data',
+              callback_data: '{"a": 11, "b": 2, "c": 3}',
+            },
+            {
+              text: 'Открыть сайт',
+              url: 'https://mini-app-dd-news.vercel.app/',
+            },
+            {
+              text: 'Открыть сайт',
+              url: 'https://mini-app-dd-news.vercel.app/',
+            },
+          ],
+        ],
+      },
     });
   }
 
@@ -76,11 +121,10 @@ export class BotUpdate {
     try {
       const data = await this.ai.sendMessage(text);
       console.log(JSON.stringify(data));
-      await ctx.reply(data.map(item => item.message.content).join(". "));
+      await ctx.reply(data.map((item) => item.message.content).join('. '));
     } catch (error) {
       console.error('Error fetching data:', error);
       await ctx.reply(`Ошибка: "${JSON.stringify(error)}"`);
     }
   }
-
 }
